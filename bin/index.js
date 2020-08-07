5bc6d6f80bc0920d32ca6bc2d67971af9dbe6353
xiangbo <zxb17603147537@163.com>
2020-08-07 13:18:20 +0800

    脚手架完善新增提示选择模板下载

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
-// console.log('Hello, cli!')
-// console.log(chalk.green('init创建'))
+console.log('Hello, cli!')
+console.log(chalk.green('init创建'))
 const fs = require('fs')
 const program = require('commander')
 const download = require('download-git-repo')
@@ -9,27 +9,31 @@ const inquirer = require('inquirer')
 const ora = require('ora')
 const symbols = require('log-symbols')
 const handlebars = require('handlebars')
+const template = require('../template.json')
+let templateName =Object.keys(template) 
+
+
 program
   .version(require('../package').version, '-v, --version')
   .command('create <name>')
   .action(name => {
-    console.log(name,"111")
     if(name){
-
-      // inquirer
-      //   .prompt([
-      //     {
-      //       type: 'input',
-      //       name: 'author',
-      //       message: ''
-      //     }
-      //   ])
-      //   .then(answers => {
-      //     console.log(answers.author)
+      inquirer
+        .prompt([{
+          type: 'list',
+          message: '选择要下载的模板:',
+          name: 'template',
+          choices:templateName,
+          filter: function (val) { // 使用filter将回答变为小写
+              return val.toLowerCase();
+          }
+        }])
+        .then(answers => {
+          console.log(template[answers['template']],"####")
           const lqProcess = ora('正在创建...')
           lqProcess.start()
           download(
-            'direct:https://resafety.com/gitlab/v2/nodejs/vue-resafety-cli.git',
+            `direct:${template[answers['template']]}`,
             name,
             { clone: true },
             err => {
@@ -52,7 +56,7 @@ program
               }
             }
           )
-        // })
+        })
     }else{
       console.log(symbols.error, chalk.red("请输入包名称"))
     }
